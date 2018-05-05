bd53da6c6fcbe84f47089307ca8cf50699702cdf
Alpoise <alpoise@qq.com>
2018-05-05 15:17:57 +0800

    截止到交作业
    作业截止

--- HW2/train.py
+++ HW2/train.py
@@ -24,8 +24,10 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.01, "L2 regularization lambda (default:
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 30, "Batch Size (default: 30)")
+tf.flags.DEFINE_integer("num_epochs", 80, "Number of training epochs (default: 20)")
 tf.flags.DEFINE_integer("evaluate_every", 30, "Evaluate model on dev set after this many steps (default: 20)")
 tf.flags.DEFINE_integer("checkpoint_every", 50, "Save model after this many steps (default: 20)")
+tf.flags.DEFINE_integer("num_checkpoints",3, "Number of checkpoints to store (default: 5)")
 
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
@@ -147,3 +149,8 @@ with tf.Graph().as_default():
                 print("")
             if current_step % FLAGS.checkpoint_every == 0:
                 path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+<<<<<<< HEAD
+                print("Saved model checkpoint to {}\n".format(path))
+=======
+                print("Saved model checkpoint to {}\n".format(path))
+>>>>>>> 0782333bd108a12d9f3962020cfbb7c3457d920b
